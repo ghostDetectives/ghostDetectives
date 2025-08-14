3835a9028e302b8989513e912f0f4023732896f3
honghyobin <w2234@e-mirim.hs.kr>
2025-08-14 23:54:36 +0900

    테스트2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             button1.Location = new Point(325, 173);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(241, 192);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
