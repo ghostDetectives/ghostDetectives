b0af8c57852f73850c91d46b4a26c8eeee127000
euiJin0 <dmlwls0617@icloud.com>
2025-08-16 23:52:05 +0900

    인트로 초반

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,58 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1 = new Panel();
+            pictureBox3 = new PictureBox();
+            label1 = new Label();
+            Box = new PictureBox();
+            character = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Box).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)character).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // pictureBox1
             // 
-            button1.Location = new Point(418, 231);
-            button1.Margin = new Padding(4);
-            button1.Name = "button1";
-            button1.Size = new Size(310, 256);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            pictureBox1.Image = Properties.Resources.인트로_초반자막;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(782, 405);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
-            // button2
+            // pictureBox2
             // 
-            button2.Location = new Point(203, 29);
-            button2.Name = "button2";
-            button2.Size = new Size(281, 180);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(0, -1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(782, 405);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
             // 
-            // button3
+            // panel1
             // 
-            button3.Location = new Point(654, 231);
-            button3.Margin = new Padding(4);
-            button3.Name = "button3";
-            button3.Size = new Size(310, 256);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(Box);
+            panel1.Controls.Add(character);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(782, 405);
+            panel1.TabIndex = 2;
+            panel1.Click += panel1_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.인트로_2_탐정등장;
+            pictureBox3.Location = new Point(0, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(782, 406);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DarkOrange;
+            label1.Font = new Font("한컴 말랑말랑 Bold", 10.1999989F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Image = Properties.Resources.대사박스4;
+            label1.Location = new Point(191, 311);
+            label1.Name = "label1";
+            label1.Size = new Size(175, 22);
+            label1.TabIndex = 2;
+            label1.Text = ".... 여기는 우리 집인데....\r\n";
+            // 
+            // Box
+            // 
+            Box.Image = Properties.Resources.대사박스4;
+            Box.Location = new Point(163, 283);
+            Box.Name = "Box";
+            Box.Size = new Size(486, 97);
+            Box.SizeMode = PictureBoxSizeMode.StretchImage;
+            Box.TabIndex = 1;
+            Box.TabStop = false;
+            // 
+            // character
+            // 
+            character.Image = Properties.Resources.한여주_혼란스러운_Photoroom;
+            character.Location = new Point(30, 240);
+            character.Name = "character";
+            character.Size = new Size(142, 164);
+            character.SizeMode = PictureBoxSizeMode.Zoom;
+            character.TabIndex = 0;
+            character.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1029, 600);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(782, 403);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Margin = new Padding(4);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Box).EndInit();
+            ((System.ComponentModel.ISupportInitialize)character).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Panel panel1;
+        private PictureBox Box;
+        private PictureBox character;
+        private Label label1;
+        private PictureBox pictureBox3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System.Windows.Forms;
+using ghostDetectives.Properties;
+using Microsoft.VisualBasic.ApplicationServices;
+
 namespace ghostDetectives
 {
     public partial class Form1 : Form
@@ -5,6 +9,43 @@ namespace ghostDetectives
         public Form1()
         {
             InitializeComponent();
+
+            // 초기 상태 설정
+            pictureBox1.Visible = true; // 검은 배경 인트로 
+            pictureBox2.Visible = false; // 여주 등장 
+            pictureBox3.Visible = false;
+
+            panel1.Visible = false;
+            panel1.BackColor = Color.Transparent;
+            panel1.Parent = pictureBox2;
+
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = false;
+
+            panel1.Visible = true;
+            panel1.BringToFront();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false; // 검정 인트로
+            pictureBox2.Visible = false; // 여주 등장
+            panel1.Visible = false;
+            panel1.SendToBack();
+
+            pictureBox3.Image = Properties.Resources.인트로_2_탐정등장;
+
+            pictureBox3.Visible = true;
+            pictureBox3.BringToFront();
+
+
+        }
+
+        
     }
 }
